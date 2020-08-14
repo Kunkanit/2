01d12abe7b6d767f345526b94fce03ba1b98635b
USER <USER@LAPTOP-8UKKF9Q6>
2020-08-14 23:07:04 +0700

    Add project files.

---
+++ 2/2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define PI 3.14159
+int main() {
+	float r, area1;
+	printf("Area of circle \n");
+	printf("Input your radius : ");
+	scanf_s("%f",&r);
+	printf("Your area of circle is : %.2f",area1 = PI*r*r);
+    
+	return 0;
+}
